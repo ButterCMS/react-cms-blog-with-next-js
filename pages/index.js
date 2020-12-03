5e726665963adaad021a707db9e0ab93e432711a
orlyohreally <orli-knop@ya.ru>
2020-12-03 18:36:31 +0200

    updated content on index page

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,102 @@
-import React from 'react'
-import Link from 'next/link'
-import Butter from 'buttercms'
+import Head from "next/head";
+import Link from "next/link";
 
-const butter = Butter('your_api_token')
+import Container from "@/components/container";
+import Header from "@/components/header";
+import Layout from "@/components/layout";
 
-export default class extends React.Component {
-  static async getInitialProps ({ query }) {
-    let page = query.page || 1
-
-    const resp = await butter.post.list({ page: page, page_size: 10 })
-    return resp.data
-  }
-  render () {
-    const { next_page, previous_page } = this.props.meta
-
-    return (
-      <div>
-        {this.props.data.map((post, key) => {
-          return (
-            <div key={key}>
-              <a href={`/posts/${post.slug}`}>{post.title}</a>
+export default function Posts() {
+  return (
+    <Layout>
+      <Container>
+        <Head>
+          <title></title>
+        </Head>
+        <Header title="ButterCMS. Headless CMS you'll melt over"></Header>
+        <div class="bg-gray-50">
+          <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl lg:mr-5">
+              <span class="block">Blog Engine</span>
+              <span class="block text-indigo-600">
+                You've got better things to do than building another blog.
+              </span>
+            </h2>
+            <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
+              <div class="inline-flex rounded-md shadow">
+                <Link href="/posts">
+                  <a class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                    Preview integration
+                  </a>
+                </Link>
+              </div>
+              <div class="ml-3 inline-flex rounded-md shadow">
+                <a
+                  href="https://buttercms.com/features/#flexiblecontentmodeling-blog-engine"
+                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                >
+                  Learn more
+                </a>
+              </div>
             </div>
-          )
-        })}
-
-        <br />
-
-        <div>
-          {previous_page && (
-            <Link href={`/?page=${previous_page}`}>
-              <a>Prev</a>
-            </Link>
-          )}
-
-          {next_page && (
-            <Link href={`/?page=${next_page}`}>
-              <a>Next</a>
-            </Link>
-          )}
+          </div>
         </div>
-      </div>
-    )
-  }
+
+        <div class="bg-gray-50">
+          <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl lg:mr-5">
+              <span class="block">Pages</span>
+              <span class="block text-indigo-600">
+                Build SEO landing pages, knowledge base, news articles, and more
+                by using Page Types.
+              </span>
+            </h2>
+            <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
+              <div class="inline-flex rounded-md shadow">
+                <Link href="/study-cases">
+                  <a class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                    Preview integration
+                  </a>
+                </Link>
+              </div>
+              <div class="ml-3 inline-flex rounded-md shadow">
+                <a
+                  href="https://buttercms.com/features/#flexiblecontentmodeling-page-types"
+                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-50">
+          <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl lg:mr-5">
+              <span class="block">Collections</span>
+              <span class="block text-indigo-600">
+                Create reusable promotional content and more with Collections.
+              </span>
+            </h2>
+            <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
+              <div class="inline-flex rounded-md shadow">
+                <Link href="/faq">
+                  <a class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                    Preview integration
+                  </a>
+                </Link>
+              </div>
+              <div class="ml-3 inline-flex rounded-md shadow">
+                <a
+                  href="https://buttercms.com/features/#flexiblecontentmodeling-collections"
+                  class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  );
 }

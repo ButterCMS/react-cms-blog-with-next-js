c688d6e1556d585c722ae8ff4a884765819035b9
orlyohreally <orli-knop@ya.ru>
2020-12-03 18:36:47 +0200

    added some styling to categories page

--- pages/posts/categories.js
+++ pages/posts/categories.js
@@ -1,27 +1,32 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import Layout from "@/components/layout";
+import Header from "@/components/header";
+import Container from "@/components/container";
 import { getCategories } from "@/lib/api";
 
 export default function Categories({ categories }) {
   return (
-    <>
-      <Head>
-        <title>Post categories</title>
-      </Head>
-      <h1>Categories</h1>
-      <ul>
-        {categories.map(({ name, slug }, key) => {
-          return (
-            <li key={key}>
-              <Link href={`/posts/category/${slug}`}>
-                <a>{name}</a>
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <Layout>
+      <Container>
+        <Head>
+          <title>Post categories</title>
+        </Head>
+        <Header title="Posts categories"></Header>
+        <ul>
+          {categories.map(({ name, slug }, key) => {
+            return (
+              <li key={key} className="mb-5">
+                <Link href={`/posts/category/${slug}`}>
+                  <a className="text-lg leading-6 font-medium">{name}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </Container>
+    </Layout>
   );
 }
 
